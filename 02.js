2cc9fb4c6dbf198aae014551c14cb27e71f43858
bronksbc <luis.oliveiraps@hotmail.com>
2022-06-28 10:26:51 -0300

    first commit

---
+++ 02.js
@@ -0,0 +1,16 @@
+var fibonacci = [];
+fibonacci[0] = 0;
+fibonacci[1] = 1;
+for (var i = 2; i < 15; i++) {
+fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+}
+console.log(fibonacci);
+function verificaFibonnacci (4){
+    
+    if (fibonacci.indexOf() === -1) {
+        console.log("não pertence");
+    } else {
+        console.log("pertence");
+    }    
+}
+
